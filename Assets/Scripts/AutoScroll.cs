e60d89c0119a0fd7013493652431b8283a6dc235
Yevhenii Postoienko <postoinko@gmail.com>
2022-12-26 00:10:59 +0200

    Create repo, create second scene, footer, header, horizontal scollbar

---
+++ Assets/Scripts/AutoScroll.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoScroll : MonoBehaviour
+{
+    private LoopScrollRect scrollComponent;
+    void Start()
+    {
+        scrollComponent = GetComponent<LoopScrollRect>();
+    }
+
+    void Update()
+    {
+        if (!scrollComponent.isManualScroll)
+            scrollComponent.content.position += Vector3.left;
+    }
+}
---
+++ Assets/Scripts/LoopScrollRect.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class LoopScrollRect : ScrollRect
+{
+        private bool hasOverflow = false;
+        public bool isManualScroll;
+    
+        public override void OnDrag(PointerEventData eventData)
+        {
+            base.OnDrag(eventData);
+            if (hasOverflow)
+            {
+                OnEndDrag(eventData);
+                OnBeginDrag(eventData);
+            }
+        }
+    
+        public override void OnEndDrag(PointerEventData eventData)
+        {
+            base.OnEndDrag(eventData);
+            isManualScroll = false;
+        }
+
+        public override void OnBeginDrag(PointerEventData eventData)
+        {
+            base.OnBeginDrag(eventData);
+            isManualScroll = true;
+        }
+
+        protected override void SetContentAnchoredPosition(Vector2 position)
+        {
+            hasOverflow = false;
+            
+            var offset = Vector2.zero;
+            
+            Vector2 min = m_ContentBounds.min;
+            Vector2 max = m_ContentBounds.max;
+    
+    
+            float maxOffset = viewRect.rect.max.x - max.x;
+            float minOffset = viewRect.rect.min.x - min.x;
+    
+            if (minOffset < -0.001f)
+            {
+                offset.x = -minOffset + maxOffset;
+                hasOverflow = true;
+            }
+            else if (maxOffset > 0.001f)
+            {
+                offset.x = -maxOffset + minOffset;
+                hasOverflow = true;
+            }
+    
+            base.SetContentAnchoredPosition(position + offset);
+        }
+        
+        protected override void LateUpdate()
+        {
+            var vel = velocity;
+            base.LateUpdate();
+            if (hasOverflow)
+            {
+                /*SetContentAnchoredPosition(content.anchoredPosition);*/
+                velocity = vel;
+            }
+            
+        }
+}
